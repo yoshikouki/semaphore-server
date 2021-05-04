3a1b0faa236b912a0f922dd44cdb068ddfa6d5c3
yoshikouki <yoshikouki@pepabo.com>
2021-05-04 11:34:36 +0900

    Add integration test

---
+++ integration_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+const testHost = "http://localhost:1323"
+
+func TestServerConnection(t *testing.T) {
+	res, err := http.Get(testHost + "/ping")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err := res.Body.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Server returned wrong status code: got %d want %d", res.StatusCode, http.StatusOK)
+	}
+
+	expected := "pong"
+	got := string(body)
+	if got != expected {
+		t.Errorf("Server returned wrong body: got %s want %s", got, expected)
+	}
+}
