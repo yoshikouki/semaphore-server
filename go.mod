385ea0945eb2490775d42fe238a73b6a3afb9b98
yoshikouki <yoshikouki@pepabo.com>
2021-05-04 10:53:40 +0900

    Remove Gin and Build Echo

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/yoshikouki/semaphore-server
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.1
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/labstack/echo/v4 v4.2.2
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
--- server/server.go
+++ server/server.go
@@ -1,18 +1,21 @@
 package server
 
 import (
-	"github.com/gin-gonic/gin"
+	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
 func Launch() error {
-	router := gin.Default()
-	router.GET("/ping", pong)
-	return router.Run()
+	e := echo.New()
+	e.GET("/ping", pong)
+
+	if err := e.Start(":1323"); err != nil {
+		e.Logger.Fatal(err)
+		return err
+	}
+	return nil
 }
 
-func pong(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	})
+func pong(c echo.Context) error {
+	return c.String(http.StatusOK, "pong")
 }
