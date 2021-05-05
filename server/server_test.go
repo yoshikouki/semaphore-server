8dcbcbaf339cb19c1830668f6e145a6f9b1f18ce
yoshikouki <yoshikouki@pepabo.com>
2021-05-05 10:41:58 +0900

    Create server_test.go

---
+++ server/server_test.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_ping(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/ping", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// Assertions
+	if assert.NoError(t, ping(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "pong", rec.Body.String())
+	}
+}
