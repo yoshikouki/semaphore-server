0f5292bf892ed43cbc8310bfd98371c5664c1ca9
yoshikouki <yoshikouki@gmail.com>
2021-05-07 23:11:24 +0900

    lock API のテストを追加。ヘルパーメソッドを STNS から流用した

--- api/api_test.go
+++ api/api_test.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 	"net/http"
@@ -8,10 +10,43 @@ import (
 	"testing"
 )
 
-func TestPing(t *testing.T) {
+func dummyContext(
+	t *testing.T,
+	reqType string,
+	reqPath string,
+	args interface{},
+) (echo.Context, *httptest.ResponseRecorder) {
+	var rp []byte
+	var jsonErr error
+
+	if args != nil {
+		switch args.(type) {
+		case string:
+			rp = []byte(args.(string))
+		default:
+			rp, jsonErr = json.Marshal(args)
+			assert.NoError(t, jsonErr)
+		}
+	}
+
+	req := httptest.NewRequest(reqType, reqPath, bytes.NewReader(rp))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+
+	e := echo.New()
+	ctx := e.NewContext(req, rec)
+
+	// WIP: Create Stub
+	//b, _ := model.NewModelDummy()
+	//ctx.Set(middleware.ModelKey, b)
+
+	return ctx, rec
+}
+
+func Test_ping(t *testing.T) {
 	// Setup
 	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "/ping", nil)
+	req := httptest.NewRequest(http.MethodGet, "/semaphore/ping", nil)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
 
