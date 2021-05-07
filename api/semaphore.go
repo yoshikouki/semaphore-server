e0dba20a0a254f2277d30aa52f9a397480073ba5
yoshikouki <yoshikouki@gmail.com>
2021-05-07 23:32:39 +0900

    API /lock のPOSTデータの型を定義して TTL をパースさせた

--- api/semaphore.go
+++ api/semaphore.go
@@ -3,14 +3,31 @@ package api
 import (
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"time"
 )
 
 type response struct {
 	message string
 }
 
+type LockIfNotExistsParams struct {
+	LockTarget string `json:"lock_target"`
+	User       string `json:"user"`
+	TTL        string `json:"ttl"`
+}
+
 // lockIfNotExists is Mutex what can only be used to maintain atomicity, if key don't exists.
-// key: `org-repo-stageName`
+// key: `org-repo-stage`
 func lockIfNotExists(c echo.Context) error {
-	return c.String(http.StatusOK, "test: #lockIfNotExists")
+	params := LockIfNotExistsParams{}
+	if err := c.Bind(&params); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	t, err := time.ParseDuration(params.TTL)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	return c.String(http.StatusOK, t.String())
 }
