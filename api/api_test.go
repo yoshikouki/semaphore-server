588231c95340e303116d9d65b7e42a60f9760f0f
yoshikouki <yoshikouki@pepabo.com>
2021-05-24 22:44:26 +0900

    使っていないテスト用関数を削除

--- api/api_test.go
+++ api/api_test.go
@@ -1,42 +1,12 @@
 package api
 
 import (
-	"bytes"
-	"encoding/json"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
-
-func dummyContext(
-	t *testing.T,
-	reqType string,
-	reqPath string,
-	args interface{},
-) (echo.Context, *httptest.ResponseRecorder) {
-	var rp []byte
-	var jsonErr error
-
-	if args != nil {
-		switch args.(type) {
-		case string:
-			rp = []byte(args.(string))
-		default:
-			rp, jsonErr = json.Marshal(args)
-			assert.NoError(t, jsonErr)
-		}
-	}
-
-	req := httptest.NewRequest(reqType, reqPath, bytes.NewReader(rp))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	e := echo.New()
-	ctx := e.NewContext(req, rec)
-
-	return ctx, rec
-}
 
 func Test_ping(t *testing.T) {
 	// Setup
