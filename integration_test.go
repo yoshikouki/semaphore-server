403b3ece9c9f7c707e233e1fbf88b98d0f4c5a7c
yoshikouki <yoshikouki@gmail.com>
2021-05-27 23:31:12 +0900

    fix test

--- integration_test.go
+++ integration_test.go
@@ -17,7 +17,7 @@ func TestMain(m *testing.M) {
 }
 
 func TestServerConnection(t *testing.T) {
-	res, err := http.Get(testURL + "/ping")
+	res, err := http.Get(testURL + "/health-check")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -28,35 +28,13 @@ func TestServerConnection(t *testing.T) {
 	}
 
 	if res.StatusCode != http.StatusOK {
-		t.Errorf("Server returned wrong status code: got %d want %d", res.StatusCode, http.StatusOK)
+		t.Errorf("/health-check returned wrong status code: got %d want %d", res.StatusCode, http.StatusOK)
 	}
 
 	expected := "pong"
 	got := string(body)
 	if got != expected {
 		t.Errorf("Server returned wrong body: got %s want %s", got, expected)
-	}
-}
-
-func TestRedisConnection(t *testing.T) {
-	res, err := http.Get(testURL + "/redis/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err := res.Body.Close(); err != nil {
-		t.Fatal(err)
-	}
-
-	if res.StatusCode != http.StatusOK {
-		t.Errorf("Redis returned wrong status code: got %d want %d", res.StatusCode, http.StatusOK)
-	}
-
-	expected := "PONG"
-	got := string(body)
-	if got != expected {
-		t.Errorf("Redis returned wrong body: got %s want %s", got, expected)
 	}
 }
 
